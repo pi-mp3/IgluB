9770ca5f0fce28cab891f9a0be9255b12211b12b
HelkinJimenezM <165401178+HelkinJimenezM@users.noreply.github.com>
2025-11-25 21:53:56 -0500

    Delete src/routes/chat.routes.ts

--- src/routes/chat.routes.ts
+++
@@ -1,7 +0,0 @@
-// routes/chat.routes.ts
-import { Router } from 'express';
-import { getChatMessages } from '../controllers/chat.controller';
-
-const router = Router();
-router.get('/', getChatMessages);
-export default router;
