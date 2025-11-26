fd65235801837ee85bc9c164651ada44d8ad3491
Hj <helkin.jimenez@correounivalle.edu.co>
2025-11-25 20:48:18 -0500

    render request

--- src/routes/meeting.routes.ts
+++
@@ -1,7 +0,0 @@
-// routes/meeting.routes.ts
-import { Router } from 'express';
-import { createMeeting } from '../controllers/meeting.controller';
-
-const router = Router();
-router.post('/create', createMeeting);
-export default router;
